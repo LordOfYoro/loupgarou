3769dd293c52f450c7211c8471010792eef1f11e
LordOfYoro <dev.bogaert.dylan@gmail.com>
2017-07-04 12:17:06 +0200

    Commentaire + fix bug channel et users message

--- public/js/client.js
+++ public/js/client.js
@@ -2,36 +2,36 @@
 		var channel;
 		var socket = io();
 		$('form').submit(function(){
-		  channel = $("#channel option:selected").text() ;
-		  socket.emit('channel', channel);
-		  socket.emit('chat message', $('#m').val());
-		  $('#m').val('');
+		  channel = $("#channel option:selected").text() ;		// quel channel selectionn?
+		  socket.emit('channel', channel);			//envoie du channel ? retenir pour envoie
+		  socket.emit('chat message', $('#m').val());		// envoie du message ? envoyer
+		  $('#m').val('');								// Vide le input pour r?crire
 		  return false;
 		});
 		
-		var pseudo = prompt('Quel est votre pseudo ?');
-		socket.emit('petit_nouveau', pseudo);
+		var pseudo = prompt('Quel est votre pseudo ?');	//Demande du pseudo
+		socket.emit('petit_nouveau', pseudo);			// Envoie pseudo ? retenir
 		
 		socket.on('users', function(users){
-			$('#listUsers').empty('li');
+			$('#listUsers').empty('li');   	// on vide la liste des utilisateurs avant de remplir
 			
 			var nbUsers = users.length;
 			var liCreate ;
-			
-			
+	
+	// On ajoute tous les utilisateurs sur le site
 			for(var i=0; i < nbUsers; i++){
 				liCreate = $('<li>') ;
 				liCreate.text(users[i].pseudo);
 				$('#listUsers').append(liCreate);
 			}
-		
 		});
 		
-		socket.on('chat message', function(msg){
+		// Ajout du message sur le site
+		socket.on('chat message', function(msg, channelParam){
 			var liCreate ;
 			liCreate = $('<li>') ;
 			liCreate.text(msg);
-			liCreate.prepend('<span class="'+ channel +'"> ['+  channel +'] </span>');
+			liCreate.prepend('<span class="'+ channelParam +'"> ['+  channelParam +'] </span>');
 			
 		  $('#listMessages').append(liCreate);
 		});
--- server.js
+++ server.js
@@ -6,55 +6,56 @@ var path = require('path');
 var publicDir = "/public";
 var users = [];
 
+// Ajout des css et javascript ect .. 
 app.use('/static', express.static(path.join(__dirname + publicDir)));
 
+// Répertoire
 app.get('/', function(req, res){
-	
 	var myPath = path.resolve(__dirname + "/public/index.html")
 	res.sendFile(myPath);
 });
 
 
 io.on('connection', function(socket, pseudo, channel){
-	
+	// Constructeurs pour l'utilisateur
 	function Users(id, pseudo){
 		this.id = id ;
 		this.pseudo = pseudo;
 	}
 	
-	
+	// Message envoyé pour chat
 	io.emit('some event', { for: 'everyone' });
-	socket.on('chat message', function(msg){
-		io.emit('chat message',socket.pseudo + " : " + msg);
+	socket.on('chat message', function(msg, channelParam){
+		io.emit('chat message',socket.pseudo + " : " + msg, socket.channel);
 	});
   
     // Dès qu'on nous donne un pseudo, on le stocke en variable de session
     socket.on('petit_nouveau', function(pseudo) {
+		
+		// stock l'utilisateur dans une variable global contenu dans le socket
         socket.pseudo = pseudo;	
 		
+		// Création et ajout de l'utilisateur dans le tableau
 		var utilisateur = new Users(socket.id, socket.pseudo);
 		users.push(utilisateur);
 		io.emit('users', users);
     });  
 	
-	socket.on('utilisateur', function(pseudo) {
-        io.emit('utilisateur', pseudo);		
-    });  
+
 	
     socket.on('channel', function(channel) {
+		// variable global channel contenue dans le socket
         socket.channel = channel;
     });
 	
 	socket.on('disconnect', function(){
-		// recuperation index dans le tableau de l'utilisateur actuel
-		var usersIndex;
-		
-		
-		for(var i=0; i < users.length -1; i++){
+		//  compare les id et delete dans le tableau le bon utilisateur
+		for(var i=0; i < users.length; i++){
 			if(users[i].id == socket.id ){
 				users.splice(i, 1) ;
 			}
 		}		
+		
 		io.emit('users', users);
 	});
 });
